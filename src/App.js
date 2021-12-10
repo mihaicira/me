1edffb1b30b39cbb469df0e3c979d2416aa60cc3
mihaicira <70019764+mihaicira@users.noreply.github.com>
2021-12-10 18:00:59 +0200

    17

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 // import {Suspense} from 'react';
 // import HomePage from "./Components/HomePage/HomePage";
 // import DynamicContainer from "./Components/Dynamic/DynamicContainer";
-
+import './App.scss'
 import HomePage from "./Components/HomePage/HomePage";
 
 export default  function App() {
