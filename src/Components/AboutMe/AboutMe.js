671d17264f8bc547bdd4fedacc4d9c551c2f6ac4
mihaicira <70019764+mihaicira@users.noreply.github.com>
2021-12-10 17:41:28 +0200

    9

---
+++ src/Components/AboutMe/AboutMe.js
@@ -0,0 +1,16 @@
+import "./AboutMe.scss"
+import HomeButton from "../HomeButton/HomeButton";
+import PageTitle from "../PageTitle/PageTitle";
+
+export default function AboutMe(){
+    return(
+        <>
+          <HomeButton/>
+          <PageTitle text="Hello"/>
+
+            <h1> p1</h1>
+            <h1> p4</h1>
+            <h1> p3</h1>
+        </>
+    )
+}
