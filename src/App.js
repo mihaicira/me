411f4c63134e89a504e93e922cdf64155c81ddd9
mihaicira <70019764+mihaicira@users.noreply.github.com>
2021-12-10 16:32:56 +0200

    6

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import {Canvas} from "@react-three/fiber";
 import {Suspense} from 'react';
 import HomePage from "./Components/HomePage/HomePage";
 import DynamicContainer from "./Components/Dynamic/DynamicContainer";
+import Dynamic from "./Dynamic";
 
 export default  function App() {
   return (
       <>
-          <DynamicContainer/>
+          <Suspense fallback={<div style={{color:"red"}}>Loading... </div>}>
+              <Canvas>
+                  {/*<OrbitControls />*/}
+
+                  <ambientLight intensity={0.5}/>
+                  <pointLight position={[5, 20, 30]} />
+                  <camera/>
+
+                  <Dynamic/>
+              </Canvas>
+          </Suspense>
       </>
 
   );
