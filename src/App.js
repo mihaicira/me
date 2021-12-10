8a152ba04daceb578bbbedf88d67cb32cda92a12
mihaicira <70019764+mihaicira@users.noreply.github.com>
2021-12-10 16:28:26 +0200

    5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import {Canvas} from "@react-three/fiber";
 import {Suspense} from 'react';
 import HomePage from "./Components/HomePage/HomePage";
+import DynamicContainer from "./Components/Dynamic/DynamicContainer";
 
 export default  function App() {
   return (
       <>
-          <HomePage/>
+          <DynamicContainer/>
       </>
 
   );
