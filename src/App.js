782df3bd1c9f777596125844318f05dc4e4e7af4
mihaicira <70019764+mihaicira@users.noreply.github.com>
2021-12-10 18:09:36 +0200

    19

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@
 // import DynamicContainer from "./Components/Dynamic/DynamicContainer";
 import './App.scss'
 import HomePage from "./Components/HomePage/HomePage";
+import StarfieldAnimation from "react-starfield-animation";
 
 export default  function App() {
   return (
       <>
+          <StarfieldAnimation
+              style={{
+                  position: 'absolute',
+                  width: '100%',
+                  height: '100%'
+              }}
+              lineWidth={3}
+              numParticles={500}
+          />
           <HomePage/>
       </>
 
