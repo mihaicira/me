f7b22fa058a40dd8dda1a5454d3f3d68d2975a89
mihaicira <70019764+mihaicira@users.noreply.github.com>
2021-12-10 18:04:06 +0200

    18

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ export default  function App() {
   return (
       <>
           <HomePage/>
-          <h2>hello</h2>
       </>
 
   );
--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,3 +1,4 @@
+import "./HomePage.scss"
 import Dynamic from "../../Dynamic";
 import {Suspense} from 'react';
 import {Canvas} from "@react-three/fiber";
@@ -6,18 +7,36 @@ import {Canvas} from "@react-three/fiber";
 function HomePage() {
     return (
         <>
-            <Suspense fallback={<div style={{color:"red"}}>Loading... </div>}>
-                <Canvas>
-                    {/*<OrbitControls />*/}
+            <div className="dynamic-container">
+                <Suspense fallback={<div style={{color:"red"}}>Loading... </div>}>
+                    <Canvas>
+                        {/*<OrbitControls />*/}
 
-                    <ambientLight intensity={0.5}/>
-                    <pointLight position={[5, 20, 30]} />
-                    <camera/>
+                        <ambientLight intensity={0.5}/>
+                        <pointLight position={[5, 20, 30]} />
+                        <camera/>
 
-                    <Dynamic/>
-                </Canvas>
-            </Suspense>
-            <h2>lol hi</h2>
+                        <Dynamic/>
+                    </Canvas>
+                </Suspense>
+            </div>
+
+            <div className="main-content-container">
+
+                <div className="main-left-container">
+                    <h1>Cîra Mihai</h1>
+                    <h6>web developer</h6>
+                    <h6>mobile developer</h6>
+                    <h6>problem solver</h6>
+                </div>
+
+                <div className="main-navbar">
+                    <button>About me</button>
+                    <button>Projects</button>
+                    <button>Skills</button>
+                    <button>Contact</button>
+                </div>
+            </div>
 
 
         </>
