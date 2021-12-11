25d67a3b0a3695431d794076cc08d266a5cee07a
mihaicira <70019764+mihaicira@users.noreply.github.com>
2021-12-11 04:31:04 +0200

    contact

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,9 +2,13 @@ import "./Contact.scss"
 import HomeButton from "../HomeButton/HomeButton";
 import {useEffect, useState,useRef} from 'react';
 import PageTitle from "../PageTitle/PageTitle";
+import emailjs from "emailjs-com";
 
 export default function Contact(){
     const [pageOpacity,setPageOpacity] = useState(0)
+    const [email,setEmail] = useState("")
+    const [message,setMessage] = useState("")
+    const [sent,setSent] = useState()
     const element = useRef()
 
 
@@ -15,11 +19,49 @@ export default function Contact(){
         },250)
     },[])
 
+    const handleForm = (e)=>{
+        if(message !== '' && email !== ''){
+            e.preventDefault()
+            const date = new Date()
+
+            setSent(true)
+
+            //Send email
+            emailjs.send("service_rzi5f25","template_58vtlaj",{
+                email: email,
+                message: message,
+                date: date.toString()
+            },'user_T4jIQ0R5325FA0lZ8gMtO');
+        }
+    }
+
     return(<>
         <div id="contact-page" className="subpage" ref={element} style={{"opacity":`${pageOpacity}`}}>
             <HomeButton page="contact-page"/>
             <PageTitle text="Contact"/>
-            <h2>heya</h2>
+            <h3>Complete the form below or send me an email at <a href="mailto:ciramihai291@gmail.com">ciramihai291@gmail.com</a></h3>
+
+            <div>
+                <form onSubmit={handleForm} id="contact-form">
+                    {
+                        !sent &&
+                            <>
+                                <label>Your name</label>
+                                <input type="text" onChange={e=>setEmail(e.target.value)} required/>
+
+                                <label>Your name</label>
+                                <textarea  onChange={e=>setMessage(e.target.value)} required/>
+
+                                <button>Submit</button>
+                            </>
+                    }
+                    {
+                        sent &&
+                            <h2>Your message has been sent. Thank you!</h2>
+                    }
+
+                </form>
+            </div>
         </div>
     </>)
 }
