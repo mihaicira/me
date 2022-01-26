c6e7956889c80f151919e317f7489789f3e0eb6d
mihaicira <70019764+mihaicira@users.noreply.github.com>
2022-01-26 18:42:32 +0200

    description updated

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -33,17 +33,12 @@ export default function AboutMe(){
                     <div>
                         <h5>$whoami</h5>
                         <p>
-                            A highly motivated 3rd year Computer Science student looking for
-                            experience to learn, be challenged and develop my skills. I have been
-                            coding since highschool, building a great variety of programs, such as
-                            Sudoku solvers and various games using C++ or Python. I participated
-                            in the Zero Robotics Competition and managed to qualify in the semifinals, with my team made up of fellow classmates. I have taught Python
-                            language to a group of 9th grade students. Beside that, I am a web
-                            development enthusiast who enjoys creating interesting UI and UX
-                            designs. I am also currently involved in some university web projects as
-                            a full-stack developer, being the team leader of a bigger project at my
-                            university.
-
+                            A highly motivated 3rd-year Computer Science student looking for experience to learn, be challenged, and develop my skills.
+                            I have been coding since high school, building a great variety of programs, such as Sudoku solvers and various games using C++ or Python.
+                            I participated in the Zero Robotics Competition and managed to qualify in the semi-finals, with my team made up of fellow classmates.
+                            At the beginning of 2020, I have taught Python language to a group of 9th-grade students.
+                            I have experience in web development, mobile development, and software development and I'm mainly interested in finding a junior position as a web developer
+                            that will take advantage of my acknowledgment and help me grow professionally. I am involved in many web projects and I'm looking forwards to being part of as many interesting projects as I can.
                         </p>
                     </div>
 
